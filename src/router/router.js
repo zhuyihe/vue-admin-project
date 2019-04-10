504ce305e3f461976c49b3101c32371f6d46f02a
zhuyihe <zhuyihe@jubao56.com>
2019-04-10 10:57:41 +0800

    新增人保权限

--- src/router/router.js
+++ src/router/router.js
@@ -2,95 +2,105 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import store from '../store/store'
 import NProgress from 'nprogress' //进度条
-import 'nprogress/nprogress.css' 
+import 'nprogress/nprogress.css'
 Vue.use(Router)
 // 路由懒加载
-const getComponent = (name,component) => () => import(`@/views/${name}/${component}.vue`);
-const myRouter=new Router({
+const getComponent = (name, component) => () => import(`@/views/${name}/${component}.vue`);
+const myRouter = new Router({
   routes: [
     {
       path: '/',
       redirect: '/home',
-      component: getComponent('login','index')
+      component: getComponent('login', 'index')
     },
     {
       path: '/login',
       name: 'login',
-      component: getComponent('login','index')
+      component: getComponent('login', 'index')
     },
     {
       path: '/',
-      component:getComponent('layout','Layout'),
-      children:[{
-        path:'/home',
-        name:'home',
-        component: getComponent('home','index'),
-        meta:{title:'首页'}
+      component: getComponent('layout', 'Layout'),
+      children: [{
+        path: '/home',
+        name: 'home',
+        component: getComponent('home', 'index'),
+        meta: { title: '首页' }
       },
       {
-        path:'/icon',
-        component: getComponent('icons','index'),
-        name:'icon',
-        meta:{title:'自定义图标'}
+        path: '/icon',
+        component: getComponent('icons', 'index'),
+        name: 'icon',
+        meta: { title: '自定义图标' }
       },
       {
-        path:'/editor',
-        component: getComponent('component','editor'),
-        name:'editor',
-        meta:{title:'富文本编译器'}
+        path: '/editor',
+        component: getComponent('component', 'editor'),
+        name: 'editor',
+        meta: { title: '富文本编译器' }
       },
       {
-        path:'/countTo',
-        component: getComponent('component','countTo'),
-        name:'countTo',
-        meta:{title:'数字滚动'}
+        path: '/countTo',
+        component: getComponent('component', 'countTo'),
+        name: 'countTo',
+        meta: { title: '数字滚动' }
       },
       {
-        path:'/tree',
-        component: getComponent('component','tree'),
-        name:'tree',
-        meta:{title:'自定义树'}
+        path: '/tree',
+        component: getComponent('component', 'tree'),
+        name: 'tree',
+        meta: { title: '自定义树' }
       },
       {
-        path:'/treeTable',
-        component: getComponent('component','treeTable'),
-        name:'treeTable',
-        meta:{title:'表格树'}
+        path: '/treeTable',
+        component: getComponent('component', 'treeTable'),
+        name: 'treeTable',
+        meta: { title: '表格树' }
       },
       {
-        path:'/treeSelect',
-        component: getComponent('component','treeSelect'),
-        name:'treeSelect',
-        meta:{title:'下拉树'}
+        path: '/treeSelect',
+        component: getComponent('component', 'treeSelect'),
+        name: 'treeSelect',
+        meta: { title: '下拉树' }
       },
       {
-        path:'/draglist',
-        component: getComponent('draggable','draglist'),
-        name:'draglist',
-        meta:{title:'拖拽列表'}
+        path: '/draglist',
+        component: getComponent('draggable', 'draglist'),
+        name: 'draglist',
+        meta: { title: '拖拽列表' }
       },
       {
-        path:'/dragtable',
-        component: getComponent('draggable','dragtable'),
-        name:'dragtable',
-        meta:{title:'拖拽表格'}
+        path: '/dragtable',
+        component: getComponent('draggable', 'dragtable'),
+        name: 'dragtable',
+        meta: { title: '拖拽表格' }
       },
       {
-        path:'/cricle',
-        component: getComponent('charts','cricle'),
-        name:'cricle',
-        meta:{title:'饼图'}
+        path: '/cricle',
+        component: getComponent('charts', 'cricle'),
+        name: 'cricle',
+        meta: { title: '饼图' }
       },
-    ]
+      {
+        path: '/404',
+        component: getComponent('error', '404'),
+        name: '404',
+        meta: { title: '404' }
+      },
+      ]
+    },
+    {
+      path: '*',
+      redirect: '/404'
     }
   ]
 })
 //判断是否存在token
-myRouter.beforeEach((to,from,next)=>{
+myRouter.beforeEach((to, from, next) => {
   NProgress.start()
   if (to.path !== '/login' && !store.state.token) {
-     next('/login')
-     NProgress.done() // 结束Progress
+    next('/login')
+    NProgress.done() // 结束Progress
   }
   next()
 })
---
+++ src/views/error/404.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="err">
+        <img src="../../assets/img/404.png" alt="">
+        <p class="errCode">你所访问的页面不存在</p>
+        <div class="error-handle">
+          <router-link to="/">
+            <el-button type="primary" size="large">返回首页</el-button>
+          </router-link>
+          <el-button class="error-btn" type="primary" size="large" @click="back">返回上一页</el-button>
+      </div>
+    </div>    
+</template>
+<script>
+export default {
+    methods: {
+      back(){
+          this.$router.go(-1);
+      }
+  }
+}
+</script>
+<style lang="scss" scoped>
+    .err{
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        flex-direction: column;
+        width: 100%;
+        height: 100%;
+        box-sizing: border-box;
+        padding-top: 100px;
+    }
+    .errCode{
+        font-size: 20px
+    }
+    .error-handle{
+        margin-top: 30px;
+        padding-bottom: 200px;
+    }
+    .error-btn{
+        margin-left: 100px;
+    }
+</style>
